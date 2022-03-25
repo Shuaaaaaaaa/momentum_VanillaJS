043f0be1c5bfc0ea174ae8dcb89a4c9246e52745
노수아 (SHUA NOH) 사원 (SW Engineering팀) <sanoh1@seegene.com>
2022-03-25 23:03:35 +0900

    2022-03-25 Commit

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 //The way we execute .js and .css is not by opening them
 //HTML is a glue that sticks .js and .css together
 
-const a = 5;
+//Variable = let, const, var
+//const --> variable value never going to change (most of the time)
+//let --> creating a variable for the first time (sometimes)
+//(VERY OLD) var --> value can be changed, no rule exists (never use)
+
+let a = 5;
 const b = 2;
+let myName = "shuaaaa"; //Python way would be my_name
 
-console.log(5 + 2); //prints something on 'Console'
-console.log(5 * 2);
-console.log(5 / 2);
+console.log(a + b); //prints something on 'Console'
+console.log(a * b);
+console.log(a / b);
+console.log("hello "+myName);
 
+myName = "SHUA" //if const myName at line 11, ERROR
 
+console.log("hello "+myName);
+
+//Boolean = true, false, null, undefined
+const amIFat = null; //null = VALUE DOES NOT EXIST
+let something; //null != undefined
+console.log(something, amIFat); //prints 'undefined'
+
+//Arrays
+const nonsense = [1,2,"hello",false,null,true,undefined,"shua"];
+console.log(nonsense);
+console.log(nonsense[4]);
+
+nonsense.push('push');
+console.log(nonsense);
+
+//Objects
+const player = {
+    name: "shua",
+    points: 10,
+    fat: true,
+}
+console.log(player);
+console.log(player.points);
+console.log(player["points"]);
+
+player.points = 30; //works while 'const'
+player.lastname = "strawberry" //also works
+console.log(player); //player itself is still the same, we're changing the value inside player, so 'const' is right
