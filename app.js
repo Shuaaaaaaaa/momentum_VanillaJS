1323877bba95e11a79623a15d53afdac2e108212
노수아 (SHUA NOH) 사원 (SW Engineering팀) <sanoh1@seegene.com>
2022-03-25 22:32:17 +0900

    Initial Files

---
+++ app.js
@@ -0,0 +1,11 @@
+//The way we execute .js and .css is not by opening them
+//HTML is a glue that sticks .js and .css together
+
+const a = 5;
+const b = 2;
+
+console.log(5 + 2); //prints something on 'Console'
+console.log(5 * 2);
+console.log(5 / 2);
+
+
