408f1a2552f9422e5cf1c8bc36264a54fe5ff712
노수아 (SHUA NOH) 사원 (SW Engineering팀) <sanoh1@seegene.com>
2022-03-26 10:19:09 +0900

    2022-03-26 COMPLETE 1 [2021 UPDATE] INTRODUCTION 2 [2021 UPDATE] WELCOME TO JAVASCRIPT

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ const player = {
     name: "shua",
     points: 10,
     fat: true,
+    sayHello: function(stranger){
+        console.log("hello! " + stranger);
+    }
 }
 console.log(player);
 console.log(player.points);
@@ -45,3 +48,74 @@ console.log(player["points"]);
 player.points = 30; //works while 'const'
 player.lastname = "strawberry" //also works
 console.log(player); //player itself is still the same, we're changing the value inside player, so 'const' is right
+
+player.sayHello("lynn"); //built something similar to 'console.log'
+
+const calculator = {
+    add: function(a,b){
+        console.log(a+b);
+    },
+    subtract: function(a,b){
+        console.log(a-b);
+    },
+    divide: function(a,b){
+        console.log(a/b);
+    },
+    power: function(a,b){
+        console.log(a**b);
+    }
+}
+
+calculator.add(5,2);
+calculator.subtract(5,2);
+calculator.divide(5,2);
+calculator.power(5,2);
+
+//Functions
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is "+ nameOfPerson + "and I'm "+age+" years old.")
+}
+
+sayHello("shua",10);
+sayHello("dal",11);
+sayHello("lynn",12);
+
+function minusFive(potato){
+    console.log(potato-5);
+}
+minusFive(10,10,12,34,3,4,5,6); //prints 5
+
+//Returns
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner+2;
+    console.log("hi") //does not execute
+}
+
+const krAge = calculateKrAge(96);
+console.log(krAge);
+
+//Conditionals
+const age = parseInt(prompt("How old are you?")); //stops JS, we do not use this anymore
+//if age = number,
+console.log(age);
+console.log(typeof parseInt(age)); //prints "number"
+console.log(typeof age); //but prints "string", this is default
+//if age = string,
+console.log(typeof parseInt(age)); //prints "number"
+console.log(typeof age); //but prints "string", this is default
+console.log(parseInt(age)); //prints NaN
+
+
+if (isNaN(age)|| age<0){
+    console.log("Please write a number");
+}else if (age<18){
+    console.log("You are too young.");
+}else if (age >= 18 && age <= 50){
+    console.log("You can drink.");
+}else if (age > 50 && age <= 80){
+    console.log("You should exercise.");
+}else if (age==100){
+    console.log("wow you are wise.")
+}else if (age>80){
+    console.log("You can do whatever you want.")
+}
